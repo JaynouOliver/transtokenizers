3ff8e6c1269e12c8b64a127a95f0e5b08d24529e
JaynouOliver <hyb2084.usa@gmail.com>
2024-08-06 19:33:24 +0530

    deleted useless files

--- transtokenizers.py
+++
@@ -1,6 +0,0 @@
-def translate(bn_sentences, en_sentences):
-    translations = []
-    for bn, en in zip(bn_sentences, en_sentences):
-        translation = f"Translated: {bn} -> {en}"
-        translations.append(translation)
-    return translations
